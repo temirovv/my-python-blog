f1b3175b8b4a80317df22baa65ac2a9e7d0c400c
Enthony <temirovv21@gmail.com>
2021-07-14 21:17:51 +0500

    modified:   config/settings.py 	modified:   staticfiles/staticfiles.json

--- config/settings.py
+++ config/settings.py
@@ -122,8 +122,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
